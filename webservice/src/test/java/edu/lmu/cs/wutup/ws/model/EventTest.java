f9424665d01045b2cb6e890d3dd9a0740db9f585
Richard Brous <rbrousla@gmail.com>
2012-09-19 23:55:49 -0700

    added tests for owner, category, start, end and attendees

--- webservice/src/test/java/edu/lmu/cs/wutup/ws/model/EventTest.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/model/EventTest.java
@@ -6,6 +6,10 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertFalse;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class EventTest {
@@ -22,14 +26,32 @@ public class EventTest {
     @Test
     public void fieldsSetBySettersCanBeRead() {
         Event e = new Event();
+        User eventOwner = new User("Bohnam", "John", "jbohnan@gmail.com", "John");
+        DateTime timeToStart = new DateTime("2011-12-13T21:39:45.618-08:00");
+        DateTime timeToEnd = new DateTime("2011-12-14T21:39:45.618-08:00");
+        List<User> someUsers = new ArrayList<User>();
+        someUsers.add(new User("Plant", "Robert", "rplant@gmail.com", "Rob"));
+        someUsers.add(new User("Page", "Jimmy", "jpage@gmail.com", "Jim"));
         e.setId(5);
         e.setName("Programming Contest");
         e.setDescription("Student Programming Contest");
         e.setLocation("LMU");
+        e.setCategory("Premium");
+        e.setOwner(eventOwner);
+        //e.setStart(new DateTime("2011-12-13T21:39:45.618-08:00")); // also passed test
+        //e.setEnd(new DateTime("2011-12-14T21:39:45.618-08:00")); // also passed test
+        e.setStart(timeToStart);
+        e.setEnd(timeToEnd);        
+        e.setAttendees(someUsers);
         assertThat(e.getId(), is(5));
         assertThat(e.getName(), is("Programming Contest"));
         assertThat(e.getDescription(), is("Student Programming Contest"));
         assertThat(e.getLocation(), is("LMU"));
+        assertThat(e.getCategory(), is("Premium"));
+        assertThat(e.getOwner(), is(eventOwner));
+        assertThat(e.getStart(), is(timeToStart));
+        assertThat(e.getEnd(), is(timeToEnd));
+        assertThat(e.getAttendees(), is(someUsers));
     }
 
     @Test
