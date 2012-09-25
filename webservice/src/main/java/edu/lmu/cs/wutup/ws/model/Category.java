3660471b9525158be40e428813d71f6cc3a30059
Sam <verhasseltsamuelj@gmail.com>
2012-09-25 13:58:19 -0700

    -- Better Stub for Brous.

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Category.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Category.java
@@ -1,1 +1,21 @@
 package edu.lmu.cs.wutup.ws.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.joda.time.DateTime;
+
+@XmlRootElement(name = "category")
+public class Category {
+
+        private int id;
+        private String name;
+
+        public Category() {
+            // No-arg constructor required for annotations
+           }
+
+           public Category(String name) {
+               this.name = name;
+            }
+
+}
