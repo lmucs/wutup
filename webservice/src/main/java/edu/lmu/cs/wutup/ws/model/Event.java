7dbfad49523fa10b5496b1a6e7d82ef1ace730d0
Richard Brous <rbrousla@gmail.com>
2012-09-25 18:49:19 -0700

    updated Event test and class

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Event.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Event.java
@@ -11,113 +11,118 @@ import com.google.common.base.Objects;
 @XmlRootElement(name = "event")
 public class Event {
 
-    private Integer id;
-    private String name;
-    private String description;
-    // private List<Category> category = new ArrayList<Category>();
-    private User owner;
-    private List<User> attendees = new ArrayList<User>();
-    // private List<EventOccurrence> eventOccurrences = new ArrayList<EventOccurrence>();
+	private Integer id;
+	private String name;
+	private String description;
+	private User owner;
+	private List<User> attendees = new ArrayList<User>();
+	private List<EventOccurrence> eventOccurrence = new ArrayList<EventOccurrence>();
+    private List<Category> category = new ArrayList<Category>();
 
+	public Event() {
+		// No-arg constructor required for annotations
+	}
 
-    public Event() {
-        // No-arg constructor required for annotations
-    }
+	public Event(int id, String name, String description, User owner, List<User> attendees,
+			List<EventOccurrence> eventOccurrence, List<Category> category) {
 
-    public Event(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    
-    public Event(int id, String name, String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.owner = owner;
+		this.attendees = attendees;
+		this.eventOccurrence = eventOccurrence;
+		this.category = category;
+	}
 
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+	public Event(int id, String name) {
+		this(id, name, null, null, null, null, null);
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    @XmlElement(name = "id")
-    public int getId() {
-        return this.id;
-    }
+	@XmlElement(name = "id")
+	public int getId() {
+		return this.id;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    @XmlElement(name = "name")
-    public String getName() {
-        return this.name;
-    }
+	@XmlElement(name = "name")
+	public String getName() {
+		return this.name;
+	}
 
-    public void setDescription(final String description) {
-        this.description = description;
-    }
+	public void setDescription(final String description) {
+		this.description = description;
+	}
 
-    @XmlElement(name = "description")
-    public String getDescription() {
-        return this.description;
-    }
+	@XmlElement(name = "description")
+	public String getDescription() {
+		return this.description;
+	}
 
-    public void setOwner(final User owner) {
-        this.owner = owner;
-    }
+	public void setOwner(final User owner) {
+		this.owner = owner;
+	}
 
-    @XmlElement(name = "owner")
-    public User getOwner() {
-        return this.owner;
-    }
-
-	@XmlElement(name = "attendees")
-	public List<User> getAttendees() {
-		return attendees;
+	@XmlElement(name = "owner")
+	public User getOwner() {
+		return this.owner;
 	}
 
 	public void setAttendees(List<User> attendees) {
 		this.attendees = attendees;
 	}
 
-	// @XmlElement(name = "eventOccurrence")
-	// public List<EventOccurrence> getEventOccurrence() {
-	//	return eventOccurrence;
-	// }
+	@XmlElement(name = "attendees")
+	public List<User> getAttendees() {
+		return attendees;
+	}
 
-	// public void setEventOccurrence(List<EventOccurrence> eventOccurrence) {
-	// 	this.eventOccurrence = eventOccurrence;
-	// }
-	
-	// @XmlElement(name = "category")
-	// public List<Category> getCategory() {
-	//	return category;
-	// }
+	public void setEventOccurrence(List<EventOccurrence> eventOccurrence) {
+		this.eventOccurrence = eventOccurrence;
+	}
 
-	// public void setCategory(List<User> category) {
-	// 	this.category = category;
-	// }
-	
+	@XmlElement(name = "eventOccurrence")
+	public List<EventOccurrence> getEventOccurrence() {
+		return eventOccurrence;
+	}
+
+	public void setCategory(List<Category> category) {
+	this.category = category;
+	}
+
+	@XmlElement(name = "category")
+	public List<Category> getCategory() {
+	return category;
+	}
+
 	@Override
-    public String toString() {
-        return Objects.toStringHelper(this).add("id", this.id).add("name", this.name).toString();
-    }
+	public String toString() {
+		return Objects.toStringHelper(this).add("id", this.id)
+				.add("name", this.name).toString();
+	}
 
-    @Override
-    public int hashCode() {
-        return this.id;
-    }
+	@Override
+	public int hashCode() {
+		return this.id;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        boolean result = false;
+	@Override
+	public boolean equals(Object obj) {
+		boolean result = false;
 
-        if (obj != null) {
-            Event other = (Event) obj;
-            result = Objects.equal(id, other.id) && Objects.equal(name, other.name);
-        }
+		if (obj != null) {
+			Event other = (Event) obj;
+			result = Objects.equal(id, other.id)
+					&& Objects.equal(name, other.name);
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
--- webservice/src/test/java/edu/lmu/cs/wutup/ws/model/EventTest.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/model/EventTest.java
@@ -15,33 +15,41 @@ public class EventTest {
 
     @Test
     public void fieldsSetByConstructorCanBeRead() {
-        Event e = new Event(3, "Pool Party", "Party at Brous House");
+    	User eventOwner = new User(21, "John", "Bohnam", "jbohnan@gmail.com", "John");
+    	List<User> someUsers = new ArrayList<User>();
+        List<EventOccurrence> someOccurrences = new ArrayList<EventOccurrence>();
+        List<Category> someCategories = new ArrayList<Category>();
+    	Event e = new Event(3, "Pool Party", "Party at Brous House", eventOwner, someUsers, someOccurrences, someCategories );
         assertThat(e.getId(), is(3));
         assertThat(e.getName(), is("Pool Party"));
         assertThat(e.getDescription(), is("Party at Brous House"));
+        assertThat(e.getOwner(), is(eventOwner));
+        assertThat(e.getAttendees(), is(someUsers));
+        assertThat(e.getEventOccurrence(), is(someOccurrences));
+        assertThat(e.getCategory(), is(someCategories));
     }
 
     @Test
     public void fieldsSetBySettersCanBeRead() {
         Event e = new Event();
-        User eventOwner = new User(21, "Bohnam", "John", "jbohnan@gmail.com", "John");
+        User eventOwner = new User(21, "John", "Bohnam", "jbohnan@gmail.com", "John");
         List<User> someUsers = new ArrayList<User>();
-        someUsers.add(new User(22, "Plant", "Robert", "rplant@gmail.com", "Rob"));
-        someUsers.add(new User(23, "Page", "Jimmy", "jpage@gmail.com", "Jim"));
+        List<EventOccurrence> someOccurrences = new ArrayList<EventOccurrence>();
+        List<Category> someCategories = new ArrayList<Category>();
         e.setId(5);
         e.setName("Programming Contest");
         e.setDescription("Student Programming Contest");
-        // e.setCategory("Premium");
         e.setOwner(eventOwner);
-        //e.setStart(new DateTime("2011-12-13T21:39:45.618-08:00")); // also passed test
-        //e.setEnd(new DateTime("2011-12-14T21:39:45.618-08:00")); // also passed test
         e.setAttendees(someUsers);
+        e.setEventOccurrence(someOccurrences);
+        e.setCategory(someCategories);
         assertThat(e.getId(), is(5));
         assertThat(e.getName(), is("Programming Contest"));
         assertThat(e.getDescription(), is("Student Programming Contest"));
-        // assertThat(e.getCategory(), is("Premium"));
         assertThat(e.getOwner(), is(eventOwner));
         assertThat(e.getAttendees(), is(someUsers));
+        assertThat(e.getEventOccurrence(), is(someOccurrences));
+        assertThat(e.getCategory(), is(someCategories));
     }
 
     @Test
