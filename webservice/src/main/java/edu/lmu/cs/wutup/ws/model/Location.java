cdbca27cce76230b1eacc8b80018fc4c778d16d2
Allyson Pascua <allysonpascua@gmail.com>
2012-09-27 09:26:17 -0700

    Location and LocationTest have been updated

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Location.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Location.java
@@ -78,10 +78,8 @@ public class Location {
         if (obj instanceof Location) {
             Location other = Location.class.cast(obj);
             result = Objects.equal(id, other.id) &&
-                     Objects.equal(address, other.address) &&
                      Objects.equal(latitude, other.latitude) &&
-                     Objects.equal(longtitude, other.longtitude) &&
-                     Objects.equal(other.propertyMap, this.propertyMap);
+                     Objects.equal(other.longtitude, this.longtitude);
         }
 
         return result;
--- webservice/src/test/java/edu/lmu/cs/wutup/ws/model/LocationTest.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/model/LocationTest.java
@@ -1,7 +1,10 @@
 package edu.lmu.cs.wutup.ws.model;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 
 import org.junit.Test;
 
@@ -9,23 +12,41 @@ public class LocationTest {
 
     @Test
     public void fieldsSetByConstructorCanBeRead() {
-        Location loc = new Location("Gym", 33.969962, -118.4185002, 1.11);
-        assertThat(loc.getAddress(), is("Gym"));
+        Location loc = new Location(123456, "Keck Lab", 33.969962, -118.4185002, "Doolan");
+        assertThat(loc.getId(), is(123456));
+        assertThat(loc.getAddress(), is("Keck Lab"));
         assertThat(loc.getLatitude(), is(33.969962));
         assertThat(loc.getLongtitude(), is(-118.4185002));
-        assertThat(loc.getAccuracy(), is(1.11));
+        assertThat(loc.getPropertyMap(), is("Doolan"));
     }
 
     @Test
     public void fieldsSetBySettersCanBeRead() {
         Location loc = new Location();
+        loc.setId(654321);
         loc.setAddress("pool");
         loc.setLatitude(30.000001);
         loc.setLongtitude(-118.000001);
-        loc.setAccuracy(0.11);
+        loc.setPropertyMap("gym");
+        assertThat(loc.getId(), is(654321));
         assertThat(loc.getAddress(), is("pool"));
         assertThat(loc.getLatitude(), is(30.000001));
         assertThat(loc.getLongtitude(), is(-118.000001));
-        assertThat(loc.getAccuracy(), is(0.11));
+        assertThat(loc.getPropertyMap(), is("gym"));
+    }
+
+    @Test
+    public void equalsUsesIdLatitudeAndLongtitudeOnly() {
+        assertThat(new Location(1, 11.1, 22.2), equalTo(new Location(1, 11.1, 22.2)));
+        assertThat(new Location(1, 11.1, 22.2), not(equalTo(new Location(1, 22.2, 11.1))));
+        assertThat(new Location(1, 11.1, 22.2), not(equalTo(new Location(999, 11.1, 22.2))));
+        assertFalse(new Location(1, 11.1, 22.2).equals("candy"));
+        assertFalse(new Location(1, 11.1, 22.2).equals(null));
+
+    }
+
+    @Test
+    public void hashCodeProducesId() {
+        assertThat(new Location(1, 11.1, 22.2).hashCode(), is(1));
     }
 }
