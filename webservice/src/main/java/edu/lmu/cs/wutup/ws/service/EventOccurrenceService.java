e0f3d6a81bb3a3f56e5d16ae6f084747bd9b7c13
Andrew Won <andyjwon@gmail.com>
2012-09-27 06:32:39 -0700

    Added EventOccurrenceService.java

---
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/service/EventOccurrenceService.java
@@ -0,0 +1,22 @@
+package edu.lmu.cs.wutup.ws.service;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import edu.lmu.cs.wutup.ws.model.EventOccurrence;
+
+public interface EventOccurrenceService {
+
+    void createEventOccurrence(EventOccurrence e);
+
+    EventOccurrence findEventOccurrenceById(int id);
+    List<EventOccurrence> findEventOccurrencesByLocation(String location, int pageNumber, int pageSize);
+    List<EventOccurrence> findEventOccurrencesByStartTime(DateTime start, int pageNumber, int pageSize);
+    List<EventOccurrence> findAllEventOccurrences(int pageNumber, int pageSize);
+
+    void updateEventOccurrence(EventOccurrence e);
+    void deleteEventOccurrence(EventOccurrence e);
+
+    int findNumberOfEventOccurrences();
+}
