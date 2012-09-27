99bf91e7913aa3bc52f649f4a51b8ade236ee534
rtoal <rtoal@lmu.edu>
2012-09-27 09:22:05 -0700

    Flesh out some domain classes, add initial tests for the user service, etc.

--- webservice/src/test/java/edu/lmu/cs/wutup/ws/model/CommentTest.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/model/CommentTest.java
@@ -7,22 +7,32 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class CommentTest {
-    
+
+    User alice = new User(1, "alice@example.com");
+
     @Test
     public void fieldsSetByConstructorCanBeRead() {
-    	DateTime timestamp = new DateTime();
-        Comment c = new Comment("This is a comment", timestamp);
+        DateTime timestamp = new DateTime();
+        Comment c = new Comment(1, "This is a comment", timestamp, alice);
+        assertThat(c.getId(), is(1));
         assertThat(c.getBody(), is("This is a comment"));
         assertThat(c.getTimestamp(), is(timestamp));
+        assertThat(c.getAuthor(), is(alice));
     }
-    
+
     @Test
     public void fieldsSetBySettersCanBeRead() {
         Comment c = new Comment();
         DateTime timestamp = new DateTime();
+        c.setId(10);
         c.setBody("Comments are awesome");
         c.setTimestamp(timestamp);
+        c.setAuthor(alice);
+        assertThat(c.getId(), is(10));
         assertThat(c.getBody(), is("Comments are awesome"));
         assertThat(c.getTimestamp(), is(timestamp));
+        assertThat(c.getAuthor(), is(alice));
     }
+
+    // TODO tests for hashCode, equals, and toString
 }
