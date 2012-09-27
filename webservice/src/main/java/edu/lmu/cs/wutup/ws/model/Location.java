de6c9e0d7ebac04c999dec617d612ec20a222f03
Allyson Pascua <allysonpascua@gmail.com>
2012-09-27 08:20:37 -0700

    The variable accuracy is replaced by propertyMap. toString() also added.

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Location.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/model/Location.java
@@ -1,22 +1,28 @@
 package edu.lmu.cs.wutup.ws.model;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.Objects;
+
 public class Location {
     private String address;
     private double latitude;
     private double longtitude;
-    private double accuracy;
-    
-    public Location() {
+    private String propertyMap;
 
+    public Location() {
+        // No-arg constructor
     }
 
-    public Location(String address, double latitude, double longtitude, double accuracy) {
+    public Location(String address, double latitude, double longtitude, String propertyMap) {
         this.address = address;
         this.latitude = latitude;
         this.longtitude = longtitude;
-        this.accuracy = accuracy;
+        this.propertyMap = propertyMap;
     }
 
+    @XmlElement(name = "address")
     public String getAddress() {
         return address;
     }
@@ -25,6 +31,7 @@ public class Location {
         this.address = address;
     }
 
+    @XmlElement(name = "latitude")
     public double getLatitude() {
         return latitude;
     }
@@ -33,6 +40,7 @@ public class Location {
         this.latitude = latitude;
     }
 
+    @XmlElement(name = "longtitude")
     public double getLongtitude() {
         return longtitude;
     }
@@ -41,12 +49,37 @@ public class Location {
         this.longtitude = longtitude;
     }
 
-    public double getAccuracy() {
-        return accuracy;
+    @XmlElement(name = "propertymap")
+    public String getPropertyMap() {
+        return propertyMap;
     }
 
-    public void setAccuracy(double accuracy) {
-        this.accuracy = accuracy;
+    public void setPropertyMap(String propertyMap) {
+        this.propertyMap = propertyMap;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+
+        if (obj instanceof Location) {
+            Location other = Location.class.cast(obj);
+            result = Objects.equal(address, other.address) &&
+                     Objects.equal(latitude, other.latitude) &&
+                     Objects.equal(longtitude, other.longtitude) &&
+                     Objects.equal(propertyMap, other.propertyMap);
+        }
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("address", this.address)
+                .add("latitude", this.latitude)
+                .add("longtitude", this.longtitude)
+                .add("propertyMap", this.propertyMap).toString();
     }
 
 }
