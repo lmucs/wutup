dbd6b57b2cc9f21d4346a0662335d874508fe924
Richard Brous <rbrousla@gmail.com>
2012-10-02 08:48:49 -0700

    Modified tests to accomodate removal of attendees, owner and categories.

--- webservice/src/test/java/edu/lmu/cs/wutup/ws/model/EventTest.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/model/EventTest.java
@@ -7,71 +7,41 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertFalse;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 
 public class EventTest {
 
 	@Test
 	public void fieldsSetByConstructorCanBeRead() {
-		User eventOwner = new User(21, "John", "Bohnam", "jbohnan@gmail.com",
-				"John");
-		List<User> someUsers = new ArrayList<User>();
-		List<EventOccurrence> someOccurrences = new ArrayList<EventOccurrence>();
-		List<Category> someCategories = new ArrayList<Category>();
-		Event e = new Event(3, "Pool Party", "Party at Brous House",
-				eventOwner, someUsers, someOccurrences, someCategories);
+		Event e = new Event(3, "Pool Party", "Party at Brous House", 25);
 		assertThat(e.getId(), is(3));
 		assertThat(e.getName(), is("Pool Party"));
 		assertThat(e.getDescription(), is("Party at Brous House"));
-		assertThat(e.getOwner(), is(eventOwner));
-		assertThat(e.getAttendees(), is(someUsers));
-		assertThat(e.getEventOccurrence(), is(someOccurrences));
-		assertThat(e.getCategory(), is(someCategories));
+		assertThat(e.getOwnerId(), is(25));
 	}
 
 	@Test
 	public void fieldsSetBySettersCanBeRead() {
 		Event e = new Event();
-		User eventOwner = new User(21, "John", "Bohnam", "jbohnan@gmail.com",
-				"John");
-		List<User> someUsers = new ArrayList<User>();
-		List<EventOccurrence> someOccurrences = new ArrayList<EventOccurrence>();
-		List<Category> someCategories = new ArrayList<Category>();
 		e.setId(5);
 		e.setName("Programming Contest");
 		e.setDescription("Student Programming Contest");
-		e.setOwner(eventOwner);
-		e.setAttendees(someUsers);
-		e.setEventOccurrence(someOccurrences);
-		e.setCategory(someCategories);
+		e.setOwnerId(25);
 		assertThat(e.getId(), is(5));
 		assertThat(e.getName(), is("Programming Contest"));
 		assertThat(e.getDescription(), is("Student Programming Contest"));
-		assertThat(e.getOwner(), is(eventOwner));
-		assertThat(e.getAttendees(), is(someUsers));
-		assertThat(e.getEventOccurrence(), is(someOccurrences));
-		assertThat(e.getCategory(), is(someCategories));
+		assertThat(e.getOwnerId(), is(25));
 	}
 
 	@Test
 	public void toStringProducesExpectedString() {
-		Event e = new Event(3, "Pool Party");
-		String expected = "Event{id=3, name=Pool Party, description=null, owner=null, "
-				+ "attendees=null, eventOccurrence=null, category=null}";
-		List<User> someUsers = new ArrayList<User>();
-		List<EventOccurrence> someOccurrences = new ArrayList<EventOccurrence>();
-		List<Category> someCategories = new ArrayList<Category>();
-		Event e1 = new Event(3, "Pool Party", "Party at Brous House", null,
-				someUsers, someOccurrences, someCategories);
-		System.out.print(e.toString());
+		Event e = new Event(3, "Pool Party", null, null);
+		String expected = "Event{id=3, name=Pool Party, description=null, ownerId=null}";
+		Event e1 = new Event(3, "Pool Party", "Party at Brous House", null);
 		String expected1 = "Event{id=3, name=Pool Party, description=Party at Brous House, "
-				+ "owner=null, attendees=[], eventOccurrence=[], category=[]}";
+				+ "ownerId=null}";
 		assertEquals(expected, e.toString());
 		assertEquals(expected1, e1.toString());
-		// TO DO - Need to test <User> owner with actual owner instead of null.
 	}
 
 	@Test
