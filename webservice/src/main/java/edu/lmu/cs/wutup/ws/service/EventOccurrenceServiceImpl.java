725f6d90cc81e57cf1851e3d0aaf49b54d1a0c2f
Andrew Won <andyjwon@gmail.com>
2012-10-19 21:51:12 -0700

    Update EventOccurrenceService interface to reflect newly desired queries and stub out Impl

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/service/EventOccurrenceServiceImpl.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/service/EventOccurrenceServiceImpl.java
@@ -1,14 +1,19 @@
 package edu.lmu.cs.wutup.ws.service;
 
 import java.util.List;
+import java.util.ArrayList;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.lmu.cs.wutup.ws.dao.EventOccurrenceDao;
+import edu.lmu.cs.wutup.ws.model.Category;
 import edu.lmu.cs.wutup.ws.model.Comment;
 import edu.lmu.cs.wutup.ws.model.EventOccurrence;
+import edu.lmu.cs.wutup.ws.model.User;
+import edu.lmu.cs.wutup.ws.model.Venue;
 
 @Service
 @Transactional
@@ -28,17 +33,6 @@ public class EventOccurrenceServiceImpl implements EventOccurrenceService {
     }
 
     @Override
-    public EventOccurrence findEventOccurrenceById(int id) {
-        return eventOccurrenceDao.findEventOccurrenceById(id);
-    }
-
-    @Override
-    public List<EventOccurrence> findAllEventOccurrences(int pageNumber,
-            int pageSize) {
-        return eventOccurrenceDao.findAllEventOccurrences(pageNumber, pageSize);
-    }
-
-    @Override
     public void deleteEventOccurrence(EventOccurrence e) {
         eventOccurrenceDao.deleteEventOccurrence(e);
     }
@@ -46,6 +40,57 @@ public class EventOccurrenceServiceImpl implements EventOccurrenceService {
     @Override
     public int findNumberOfEventOccurrences() {
         return eventOccurrenceDao.findNumberOfEventOccurrences();
+    }
+
+    @Override
+    public EventOccurrence findEventOccurrenceById(int id) {
+        return eventOccurrenceDao.findEventOccurrenceById(id);
+    }
+
+    @Override
+    public List<EventOccurrence> findAllEventOccurrencesByAttendees(List<User> attendees, int pageNumber, int pageSize) {
+        return new ArrayList<EventOccurrence>();
+        // TODO
+    }
+
+    @Override
+    public List<EventOccurrence> findAllEventOccurrencesByCategories(List<Category> categories, int pageNumber, int pageSize) {
+        return new ArrayList<EventOccurrence>();
+        // TODO
+    }
+
+    @Override
+    public List<EventOccurrence> findAllEventOccurrencesByCenterAndRadius(double latitude, double longitude, double radius, int pageNumber, int pageSize) {
+        return new ArrayList<EventOccurrence>();
+        // TODO
+    }
+
+    @Override
+    public List<EventOccurrence> findAllEventOccurrencesByDateRange(DateTime start, DateTime end, int pageNumber, int pageSize) {
+        return new ArrayList<EventOccurrence>();
+        // TODO
+    }
+
+    @Override
+    public List<EventOccurrence> findAllEventOccurrencesByEventId(int eventId, int pageNumber, int pageSize) {
+        return new ArrayList<EventOccurrence>();
+        // TODO
+    }
+
+    @Override
+    public List<EventOccurrence> findAllEventOccurrencesByVenues(List<Venue> venues, int pageNumber, int pageSize) {
+        return new ArrayList<EventOccurrence>();
+        // TODO
+    }
+
+    @Override
+    public void registerAttendeeForEventOccurrence(User attendee) {
+        // TODO
+    }
+
+    @Override
+    public void unregisterAttendeeForEventOccurrence(User attendee) {
+        // TODO
     }
 
     @Override
