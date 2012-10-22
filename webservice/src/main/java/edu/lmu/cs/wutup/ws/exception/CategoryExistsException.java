d919149c8a1b16b23feb6c97e28aacf92ee36c76
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-22 11:55:02 -0700

    Added CategoryExistsException.

---
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/exception/CategoryExistsException.java
@@ -0,0 +1,7 @@
+package edu.lmu.cs.wutup.ws.exception;
+
+public class CategoryExistsException extends RuntimeException {
+    
+    private static final long serialVersionUID = -1211600149430412996L;
+
+}
