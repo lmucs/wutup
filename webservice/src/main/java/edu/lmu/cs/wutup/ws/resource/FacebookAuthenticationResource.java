8913d87629b29f93df13c0c6eaae88c9ce2ace89
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-23 11:25:53 -0700

    Correctly formatted FacebookAuthenticationResource.

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/resource/FacebookAuthenticationResource.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/resource/FacebookAuthenticationResource.java
@@ -18,52 +18,39 @@ import edu.lmu.cs.wutup.ws.service.FacebookAuthenticationImpl;
 @Path("/auth")
 public class FacebookAuthenticationResource {
     FacebookAuthenticationImpl FacebookAuthentication = new FacebookAuthenticationImpl();
-    
+
     @GET
     @Path("/facebook")
     public Response authenticate() {
         try {
-            String redirectURI = "https://www.facebook.com/dialog/oauth?" +
-                    "client_id=" + System.getenv("WUTUP_FB_APP_ID") +
-                    "&redirect_uri=" + URLEncoder.encode("http://localhost:8080/wutup/auth/landing", "ISO-8859-1") +
-                    "&scope=user_events,create_event" +
-                    "&state=" + Math.abs(new Random().nextInt());
-            
-            return Response
-                    .seeOther(new URI(redirectURI))
-                    .build();
+            String redirectURI = "https://www.facebook.com/dialog/oauth?" + "client_id="
+                    + System.getenv("WUTUP_FB_APP_ID") + "&redirect_uri="
+                    + URLEncoder.encode("http://localhost:8080/wutup/auth/landing", "ISO-8859-1")
+                    + "&scope=user_events,create_event" + "&state=" + Math.abs(new Random().nextInt());
+
+            return Response.seeOther(new URI(redirectURI)).build();
         } catch (Exception e) {
-            return Response
-                    .serverError()
-                    .build();
+            return Response.serverError().build();
         }
-        
+
     }
-    
+
     @GET
     @Path("/landing")
-    public Response handleFacebookAuthenticationResponse (
-            @DefaultValue("") @QueryParam("state") String state,
+    public Response handleFacebookAuthenticationResponse(@DefaultValue("") @QueryParam("state") String state,
             @DefaultValue("") @QueryParam("code") String code,
             @DefaultValue("") @QueryParam("error_reason") String errorReason,
             @DefaultValue("") @QueryParam("error") String error,
-            @DefaultValue("") @QueryParam("error_description") String errorDescription
-            ) {
-        
+            @DefaultValue("") @QueryParam("error_description") String errorDescription) {
+
         if (!error.equals("")) {
-            return Response
-                    .status(Response.Status.UNAUTHORIZED)
-                    .build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
-        
+
         try {
-            return Response
-                    .ok(FacebookAuthentication.getAccessToken(code))
-                    .build();
+            return Response.ok(FacebookAuthentication.getAccessToken(code)).build();
         } catch (Exception e) {
-            return Response
-                    .status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
