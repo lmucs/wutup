af20835666ba2ffa8dfc5e52b868ecf9cca5ae0f
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-24 13:24:54 -0700

    Moar User resource integration tests!

--- webservice/src/test/java/edu/lmu/cs/wutup/ws/resource/UserResourceIT.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/resource/UserResourceIT.java
@@ -10,23 +10,37 @@ public class UserResourceIT {
 
     @Test
     public void getUserRespondsProperly() {
-        expect().statusCode(200)
-                .contentType("application/json")
-                .body(containsString("\"id\":1"))
-                .body(containsString("\"nickname\":\"hybrid\""))
-                .when()
-                .get("/wutup/users/1");
+        expect()
+            .statusCode(200)
+        .contentType("application/json")
+            .body(containsString("\"id\":1"))
+            .body(containsString("\"nickname\":\"hybrid\""))
+        .when()
+            .get("/wutup/users/1");
     }
 
     @Test
     public void createUserOnPostToUsersResourceReturns201() {
-        given().contentType("application/json")
+        given()
+            .contentType("application/json")
             .body("{\"id\":155,\"firstname\":\"Carlos\",\"lastname\":\"Agudo\""
                     + ",\"nickname\":\"Big Sexy\",\"email\":\"cagudo@gmail.com\"}")
         .expect()
             .statusCode(201)
         .when()
             .post("/wutup/users");
+    }
+    
+    @Test
+    public void updateUserResourceReturns204OnSuccess() {
+        given()
+            .contentType("application/json")
+            .body("{\"id\":2,\"firstname\":\"Ted\",\"lastname\":\"Stark\""
+                    + ",\"nickname\":\"headless\",\"email\":\"naked@winterfell.com\"}")
+        .expect()
+            .statusCode(204)
+        .when()
+            .put("/wutup/users/2");
     }
 
 }
