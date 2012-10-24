745bf2b7cb1e071e90d525770876ff1b7d7e4fde
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-24 15:57:33 -0700

    Added an exception that is to be used when a request is made to geocode an address but the address isn't provided.

---
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/exception/NoAddressProvidedException.java
@@ -0,0 +1,7 @@
+package edu.lmu.cs.wutup.ws.exception;
+
+public class NoAddressProvidedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1108614064026408889L;
+
+}
