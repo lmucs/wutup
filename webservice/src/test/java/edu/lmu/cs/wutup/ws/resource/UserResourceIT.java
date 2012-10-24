afc4621cd811c90db89847e3ee8fbb6746f853f2
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-24 12:25:48 -0700

    Added a couple User resource integration tests.

---
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/resource/UserResourceIT.java
@@ -0,0 +1,32 @@
+package edu.lmu.cs.wutup.ws.resource;
+
+import static com.jayway.restassured.RestAssured.expect;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.Test;
+
+public class UserResourceIT {
+
+    @Test
+    public void getUserRespondsProperly() {
+        expect().statusCode(200)
+                .contentType("application/json")
+                .body(containsString("\"id\":1"))
+                .body(containsString("\"nickname\":\"hybrid\""))
+                .when()
+                .get("/wutup/users/1");
+    }
+
+    @Test
+    public void createUserOnPostToUsersResourceReturns201() {
+        given().contentType("application/json")
+            .body("{\"id\":155,\"firstname\":\"Carlos\",\"lastname\":\"Agudo\""
+                    + ",\"nickname\":\"Big Sexy\",\"email\":\"cagudo@gmail.com\"}")
+        .expect()
+            .statusCode(201)
+        .when()
+            .post("/wutup/users");
+    }
+
+}
