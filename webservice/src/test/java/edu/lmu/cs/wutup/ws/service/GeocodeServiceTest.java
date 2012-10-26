46d7a517c4f9a24848c3446448c17518da52e98f
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-26 14:23:26 -0700

    New test checks the GoogleGateway class's ability to parse the Google JSON response down to a simplified location object.

--- webservice/src/test/java/edu/lmu/cs/wutup/ws/service/GeocodeServiceTest.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/service/GeocodeServiceTest.java
@@ -1,21 +1,37 @@
 package edu.lmu.cs.wutup.ws.service;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.lmu.cs.wutup.ws.exception.NoAddressProvidedException;
+import static edu.lmu.cs.wutup.ws.model.GoogleGateway.parseJSONResponseToLocation;
 
 public class GeocodeServiceTest {
 
-    @Autowired
-    GeocodeServiceImpl service;
+    GeocodeServiceImpl service = new GeocodeServiceImpl();
 
-    // TODO: Finish this.
-    @Ignore
     @Test(expected=NoAddressProvidedException.class)
     public void geocodeBlankAddressThrowsException() {
         service.resolveAddressToLatLong("");
+    }
+    
+    @Test(expected=NoAddressProvidedException.class)
+    public void geocodeNullAddressThrowsException() {
+        service.resolveAddressToLatLong(null);
+    }
+
+    @Test
+    public void googleGatewayProperlyParsesGoogleJSONResponse() throws JSONException {
+        JSONObject j = parseJSONResponseToLocation(new JSONObject("{\"results\" : [{\"geometry\" :" +
+                "{\"location\" : {\"lat\" : 33.9682680,\"lng\" : -118.4219410}}" + 
+                "}],\"status\" : \"OK\"}"));
+        
+        assertThat(j.getDouble("lat"), is(33.9682680));
+        assertThat(j.getDouble("lng"), is(-118.4219410));
     }
     
 }
