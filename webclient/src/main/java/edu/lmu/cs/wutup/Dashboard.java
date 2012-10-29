91404dfd68e02dc329a3f3f4147293b2564d800c
Sam <verhasseltsamuelj@gmail.com>
2012-10-28 19:02:50 -0700

    Removed unnecessary commented out code and an end tag.

--- webclient/src/main/java/edu/lmu/cs/wutup/Dashboard.java
+++ webclient/src/main/java/edu/lmu/cs/wutup/Dashboard.java
@@ -8,8 +8,6 @@ public class Dashboard extends PageWrapper {
     public Dashboard(final PageParameters parameters) {
 	super(parameters);
 
-	//add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
         // TODO Add your page's components here
 
     }
