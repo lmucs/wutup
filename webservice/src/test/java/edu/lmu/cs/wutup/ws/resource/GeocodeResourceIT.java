9476a3538e89e1e64f24ed3609ef23fddebfa3c4
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-30 16:19:07 -0700

    Added GeocodeResource ITs.

--- webservice/src/test/java/edu/lmu/cs/wutup/ws/resource/GeocodeResourceIT.java
+++ webservice/src/test/java/edu/lmu/cs/wutup/ws/resource/GeocodeResourceIT.java
@@ -7,12 +7,54 @@ import org.junit.Test;
 
 public class GeocodeResourceIT {
     @Test
-    public void redirectToFacebookForAuthentication() {
+    public void geocodeAddressToLatLong() {
         expect().
             statusCode(200).
             body(containsString("\"latitude\":33.966605")).
             body(containsString("\"longitude\":-118.423562}")).
         when().
             get("/wutup/geocode?address=1+lmu+dr,+los+angeles,+ca");
+    }
+    
+    @Test
+    public void noAddressRespondsWith400() {
+        expect().
+            statusCode(400).
+        when().
+            get("/wutup/geocode?address=");
+    }
+    
+    @Test
+    public void reverseGeocodeLatLongToAddress() {
+        expect().
+            statusCode(200).
+            body(containsString("1 Loyola Marymount University Dr, " +
+                    "Loyola Marymount University, Los Angeles, CA 90045, USA")).
+        when().
+            get("/wutup/geocode/reverse?lat=33.966605&lng=-118.423562");
+    }
+    
+    @Test
+    public void noLatRespondsWith400() {
+        expect().
+            statusCode(400).
+        when().
+            get("/wutup/geocode/reverse?lng=-118.423562");
+    }
+    
+    @Test
+    public void noLngRespondsWith400() {
+        expect().
+            statusCode(400).
+        when().
+            get("/wutup/geocode/reverse?lat=33.966605");
+    }
+    
+    @Test
+    public void noCoordinatesRespondsWith400() {
+        expect().
+            statusCode(400).
+        when().
+            get("/wutup/geocode/reverse");
     }
 }
