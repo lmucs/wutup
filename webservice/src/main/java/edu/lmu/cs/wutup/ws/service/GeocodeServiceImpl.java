eafd9d9c840b3772d029d5ebc7150a8d0ceceb66
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-30 16:21:09 -0700

    Updated GeocodeServiceImpl to support reverse geocoding.

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/service/GeocodeServiceImpl.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/service/GeocodeServiceImpl.java
@@ -1,33 +1,50 @@
 package edu.lmu.cs.wutup.ws.service;
 
+import static edu.lmu.cs.wutup.ws.model.GoogleGateway.extractAddressFromJSON;
+import static edu.lmu.cs.wutup.ws.model.GoogleGateway.geocodeAddressToLatLong;
+import static edu.lmu.cs.wutup.ws.model.GoogleGateway.geocodeCoordinatesToAddress;
+import static edu.lmu.cs.wutup.ws.model.GoogleGateway.parseJSONResponseToLocation;
+
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import edu.lmu.cs.wutup.ws.exception.MalformedCoordinatesException;
 import edu.lmu.cs.wutup.ws.exception.NoAddressProvidedException;
-import edu.lmu.cs.wutup.ws.model.GoogleGateway;
 import edu.lmu.cs.wutup.ws.model.LatLong;
 
 @Service
 public class GeocodeServiceImpl implements GeocodeService {
-    public LatLong resolveAddressToLatLong(String address) {
-        
-        if (address == null || address.equals("")) {
+    public LatLong resolveAddressToLatLong(String address) throws NoAddressProvidedException {
+
+        if (address == null || address == "") {
             throw new NoAddressProvidedException();
         }
         
         try {
-            JSONObject r = GoogleGateway.parseJSONResponseToLocation(
-                    new JSONObject(
-                        GoogleGateway.geocodeAddressToLatLong(address)
-                    )
-                );
-            
+            JSONObject r = parseJSONResponseToLocation(new JSONObject(
+                    geocodeAddressToLatLong(address)));
+
             return new LatLong(r.getDouble("lat"), r.getDouble("lng"));
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String resolveLatLongToAddress(Double lat, Double lng) throws MalformedCoordinatesException {
+
+        if (lat == null || lng == null || lat < -90 || lat > 90 || lng < -180 || lng > 180) {
+            throw new MalformedCoordinatesException();
         }
         
-        return null;
+        try {
+            return extractAddressFromJSON(new JSONObject(geocodeCoordinatesToAddress(lat, lng)));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
