e494381eef85f1731d00f61d64d5f7e49cf51d10
Tyler Nichols <tyler.n.89@gmail.com>
2012-11-01 12:19:06 -0700

    Added exceptions for Facebook events.

---
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/exception/MissingUserFBIdException.java
@@ -0,0 +1,7 @@
+package edu.lmu.cs.wutup.ws.exception;
+
+public class MissingUserFBIdException extends RuntimeException {
+
+    private static final long serialVersionUID = -1337056005556359904L;
+
+}
---
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/exception/RequiredFBNameOrStartTimeMissingException.java
@@ -0,0 +1,7 @@
+package edu.lmu.cs.wutup.ws.exception;
+
+public class RequiredFBNameOrStartTimeMissingException extends RuntimeException {
+
+    private static final long serialVersionUID = -1339603685220982556L;
+
+}
