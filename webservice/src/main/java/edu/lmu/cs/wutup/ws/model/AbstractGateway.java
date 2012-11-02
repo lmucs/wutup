41365db1b38d7e2ef1d694552107706de50fbaab
Tyler Nichols <tyler.n.89@gmail.com>
2012-11-01 17:26:08 -0700

    Added post request helper method to AbstractGateway, FacebookGateway now supports pulling and pushing events from and to Facebook, cleaned up FBAuthResource a bit, and implemented Facebook event creation and retrieval in FBAuthServiceImpl.

--- webservice/src/main/java/edu/lmu/cs/wutup/ws/model/AbstractGateway.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/model/AbstractGateway.java
@@ -8,15 +8,25 @@ import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 public abstract class AbstractGateway {
 
-    protected static HttpEntity executeRequest(String url) throws ClientProtocolException, IOException {
+    protected static HttpEntity executeGetRequest(String url) throws ClientProtocolException, IOException {
         HttpClient httpclient = new DefaultHttpClient();
         HttpGet httpget = new HttpGet(url);
         HttpResponse response = httpclient.execute(httpget);
+        HttpEntity entity = response.getEntity();
+        
+        return entity;
+    }
+    
+    protected static HttpEntity executePostRequest(String url) throws ClientProtocolException, IOException {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+        HttpResponse response = httpclient.execute(httppost);
         HttpEntity entity = response.getEntity();
         
         return entity;
--- webservice/src/main/java/edu/lmu/cs/wutup/ws/model/FacebookGateway.java
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/model/FacebookGateway.java
@@ -4,18 +4,66 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
+import org.joda.time.DateTime;
+
+import edu.lmu.cs.wutup.ws.exception.FBAccessTokenMissingException;
+import edu.lmu.cs.wutup.ws.exception.MissingUserFBIdException;
+import edu.lmu.cs.wutup.ws.exception.RequiredFBNameOrStartTimeMissingException;
 
 public class FacebookGateway extends AbstractGateway {
     public static String acquireAccessToken(String code) throws ClientProtocolException, IOException {
-        return stringifyEntity(executeRequest(constructLandingUrl(code)));
+        return stringifyEntity(executeGetRequest(constructLandingUrl(code)));
     }
-    
+
+    public static String acquireUserEvents(String accessToken) throws ParseException, ClientProtocolException,
+            IOException {
+        return stringifyEntity(executeGetRequest(constructGetEventsUrl(accessToken)));
+    }
+
+    public static String createUserEvent(String accessToken, String userFBId, String name, DateTime start,
+            DateTime end, String description, String location, String FBLocationId, String privacyType)
+            throws RequiredFBNameOrStartTimeMissingException, FBAccessTokenMissingException, ParseException,
+            ClientProtocolException, IOException {
+
+        if (accessToken == null) {
+            throw new FBAccessTokenMissingException();
+        }
+        
+        if (userFBId == null) {
+            throw new MissingUserFBIdException();
+        }
+
+        if (name == null || start == null) {
+            throw new RequiredFBNameOrStartTimeMissingException();
+        }
+
+        return stringifyEntity(executePostRequest(constructPostEventUrl(accessToken, userFBId, name, start, end, description,
+                location, FBLocationId, privacyType)));
+    }
+
     private static String constructLandingUrl(String code) throws UnsupportedEncodingException {
-        return "https://graph.facebook.com/oauth/access_token?" +
-                "client_id=" + System.getenv("WUTUP_FB_APP_ID") +
-                "&redirect_uri=" + URLEncoder.encode("http://localhost:8080/wutup/auth/landing", "ISO-8859-1") +
-                "&client_secret=" + System.getenv("WUTUP_FB_APP_SECRET") +
-                "&code=" + code;
+        return "https://graph.facebook.com/oauth/access_token?" + "client_id=" + System.getenv("WUTUP_FB_APP_ID")
+                + "&redirect_uri=" + URLEncoder.encode("http://localhost:8080/wutup/auth/landing", "ISO-8859-1")
+                + "&client_secret=" + System.getenv("WUTUP_FB_APP_SECRET") + "&code=" + code;
+    }
+
+    private static String constructGetEventsUrl(String accessToken) {
+        return "https://graph.facebook.com/me/events?access_token=" + accessToken;
+    }
+
+    private static String constructPostEventUrl(String accessToken, String userFBId, String name, DateTime start,
+            DateTime end, String description, String location, String FBLocationId, String privacyType) throws UnsupportedEncodingException {
+
+        return "https://graph.facebook.com/" + userFBId + "/events?"
+                + "access_token=" + accessToken
+                + "&name=" + URLEncoder.encode(name, "ISO-8859-1")
+                + "&start_time=" + URLEncoder.encode(start.toString(), "ISO-8859-1")
+                + ((end != null) ? "&end_time=" + URLEncoder.encode(end.toString(), "ISO-8859-1")  : "")
+                + ((description != null) ? "&description=" + URLEncoder.encode(description, "ISO-8859-1") : "")
+                + ((location != null) ? "&location=" + URLEncoder.encode(location, "ISO-8859-1") : "")
+                + ((FBLocationId != null) ? "&location_id=" + URLEncoder.encode(FBLocationId, "ISO-8859-1") : "")
+                + ((privacyType != null) ? "&privacy_type=" + URLEncoder.encode(privacyType, "ISO-8859-1") : "");
     }
 }
