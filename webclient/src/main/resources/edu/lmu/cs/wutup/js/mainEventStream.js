c36e6f3b91ffba0cf4ef787354835a6791f74bc5
Allyson Pascua <allysonpascua@gmail.com>
2012-11-06 11:25:31 -0800

    Accept, Decline, and Maybe buttons added to mainEventStream.

--- webclient/src/main/resources/edu/lmu/cs/wutup/js/mainEventStream.js
+++ webclient/src/main/resources/edu/lmu/cs/wutup/js/mainEventStream.js
@@ -1,13 +1,15 @@
 onload = function () {
 
-    initializeEventQueue("mainEventStreamContainer", "mainEventStream", 12);
+    initializeEventQueue("mainEventStreamContainer", "mainEventStream", 10, true);
     initializeEventQueue("tabHosting", "hostingEventStream", 2);
     initializeEventQueue("tabAttending", "attendingEventStream", 7);
     initializeEventQueue("tabInvitedTo", "invitedToEventStream", 16);
 
 }
 
-var initializeEventQueue = function (goesHere, id, rows) {
+var initializeEventQueue = function (goesHere, id, rows, insertButtons) {
+
+    var usesButtons = insertButtons || false;
     var totalEvents = rows;
 
     var root=document.getElementById(goesHere);
@@ -17,10 +19,47 @@ var initializeEventQueue = function (goesHere, id, rows) {
     table.style.attribute = "value";
 
     var tableBody=document.createElement('tbody');
-    var event, pictureCell, picture, detailsCell, eventTitle, eventDescription, timeCell;
+    var event, buttonCell, acceptRow, acceptButton, declineRow, declineButton, maybeRow, maybeButton;
+    var pictureCell, picture;
+    var detailsCell, eventTitle, eventDescription;
+    var timeCell;
     
     for (var i = 0; i < totalEvents; i++) {
+    
         event = document.createElement("tr");
+    
+        if (usesButtons) {
+            
+            buttonCell = document.createElement("td");
+
+            acceptRow = document.createElement("tr");
+            acceptButton = document.createElement("btn");
+            acceptButton.id = id + "-acceptButton";
+            acceptButton.className = "btn btn-small";
+            acceptButton.innerHTML = "Accept";
+            acceptRow.appendChild(acceptButton);
+
+            declineRow = document.createElement("tr");
+            declineButton = document.createElement("btn");
+            declineButton.id = id + "-declineButton";
+            declineButton.className = "btn btn-small";
+            declineButton.innerHTML = "Decline";
+            declineRow.appendChild(declineButton);
+
+            maybeRow = document.createElement("tr");
+            maybeButton = document.createElement("btn");
+            maybeButton.id = id + "-maybeButton";
+            maybeButton.className = "btn btn-small";
+            maybeButton.innerHTML = "Maybe";
+            maybeRow.appendChild(maybeButton);
+            
+            buttonCell.appendChild(acceptRow);
+            buttonCell.appendChild(declineRow);
+            buttonCell.appendChild(maybeRow);
+            
+            event.appendChild(buttonCell);
+        }
+        
 
         pictureCell = document.createElement('td');
         
@@ -44,6 +83,7 @@ var initializeEventQueue = function (goesHere, id, rows) {
 
         timeCell = document.createElement("td");
         timeCell.innerHTML = "time goes here";
+        
         
         event.appendChild(pictureCell);
         event.appendChild(detailsCell);
