a14706492fbb66c19420bff3a1faa35d5b87e4de
Andrew Won <andyjwon@gmail.com>
2012-11-13 21:02:03 -0800

    Added two new Exception classes required by EventOccurrenceDao

---
+++ webservice/src/main/java/edu/lmu/cs/wutup/ws/exception/NoSuchAttendeeException.java
@@ -0,0 +1,6 @@
+package edu.lmu.cs.wutup.ws.exception;
+
+public class NoSuchAttendeeException extends RuntimeException {
+
+    private static final long serialVersionUID = -9057437048438354017L;
+}
