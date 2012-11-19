a79015e1cda914271362199ab9a59bc0585d9433
Marc Mouallem <Marc.4D@gmail.com>
2012-11-18 22:32:49 -0800

    Added libraries and event model. Also, drafted some code for plotting events.

---
+++ android/app/src/edu/lmu/cs/wutup/android/model/User.java
@@ -0,0 +1,42 @@
+package edu.lmu.cs.wutup.android.model;
+
+public class User {
+    
+    private int id;
+    private String email;
+    private String nickname;
+    private String firstname;
+    private String lastname;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+}
