b78dfe5649942956f0adf081dc907a38b1086c33
Marc Mouallem <Marc.4D@gmail.com>
2012-11-19 21:47:09 -0800

    Cleaned code (Android client).

--- android/app/src/edu/lmu/cs/wutup/android/views/TextDump.java
+++ android/app/src/edu/lmu/cs/wutup/android/views/TextDump.java
@@ -1,11 +1,16 @@
 package edu.lmu.cs.wutup.android.views;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import edu.lmu.cs.wutup.andoird.communication.GetOccurrences;
 import edu.lmu.cs.wutup.android.manager.R;
+import edu.lmu.cs.wutup.android.model.Occurrence;
 
 public class TextDump extends Activity {
     
@@ -25,7 +30,17 @@ public class TextDump extends Activity {
 
                 
         
-            new GetOccurrences().execute();
+        AsyncTask<Void, Integer, ArrayList<Occurrence>> blarkar = new GetOccurrences().execute();
+        
+        try {
+            ArrayList<Occurrence> naspa = blarkar.get();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
         
         
