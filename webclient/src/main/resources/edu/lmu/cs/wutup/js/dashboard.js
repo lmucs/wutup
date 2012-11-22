9c1c6cc504692704804d6005987f46ac0ae9bc0c
Sam <verhasseltsamuelj@gmail.com>
2012-11-21 16:36:16 -0800

    Modified Dashboard to better fit code standards (JSLINT). Made GET calls use base URl.

--- webclient/src/main/resources/edu/lmu/cs/wutup/js/dashboard.js
+++ webclient/src/main/resources/edu/lmu/cs/wutup/js/dashboard.js
@@ -1,193 +1,201 @@
-$(document).ready(function() {
-	var map, infowindow;
-	var instantiateMapAndCalendar = function() {
-		var initialLocation, siberia = new google.maps.LatLng(60, 105), newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687), browserSupportFlag = new Boolean(), calendarEvents = null, mapOptions = {
-			zoom : 10,
-			mapTypeId : google.maps.MapTypeId.ROADMAP
-		},
-		mapMarkers = [];
-		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+$(document).ready(function () {
+	"use strict";
+    var map, infowindow, baseUrl = window.location.protocol + "//" + window.location.hostname,
+        calculateRadius = function (bounds) {
+            if (Number.prototype.toRad === undefined) {
+                Number.prototype.toRad = function () {
+                    return this * Math.PI / 180;
+                };
+            }
+            var R = 3959, // Statute Mile
+                center = bounds.getCenter(),
+                corner = bounds.getNorthEast(),
+                lat1 = center.lat().toRad(),
+                lat2 = corner.lat().toRad(),
+                dLat = (lat2 - lat1).toRad(),
+                dLon = (corner.lng() - center.lng()).toRad(),
+                a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2),
+                c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            return R * c;
+        },
 
-		// Trying W3C Geolocation
-		if (navigator.geolocation) {
-			browserSupportFlag = true;
-			navigator.geolocation.getCurrentPosition(function(position) {
-				initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-				map.setCenter(initialLocation);
-				google.maps.event.addListenerOnce(map, 'idle', function() {
-				createCalendar(calendarEvents, map, mapMarkers);
-				});
-				var bounds, radius, center;
-				google.maps.event.addListener(map, 'tilesloaded', function() {
-					bounds = map.getBounds();
-					center = bounds.getCenter();
-					radius = calculateRadius(bounds);
-					$.getJSON(generateOccurrenceURL(center,radius,getCalendarView().start,getCalendarView().end), function(occurrences) {
-						calendarEvents = parseOccurrencesForCalendar(occurrences);
-						populateCalendar(calendarEvents);
-						populateMap(map, calendarEvents, mapMarkers);
-					});
+        generateOccurrenceURL = function (center, radius, start, end) {
+            return baseUrl + ':8080/wutup/occurrences?page=0&pageSize=20&center=' + center.lat() + ',' + center.lng() + '&radius=' + (radius <= 100 ? radius : 100) + '&start=' + start.getTime() +
+                '&end=' + end.getTime();
+        },
 
-				});
+        displayInfoWindow = function (occurrence) {
+            if (infowindow) {
+                infowindow.close();
+            }
+            infowindow = new google.maps.InfoWindow({
+                content: occurrence.event.name + '</br>',
+                position: new google.maps.LatLng(occurrence.venue.latitude, occurrence.venue.longitude)
+            });
+            infowindow.open(map);
+        },
 
-			}, function() {
-				handleNoGeolocation(browserSupportFlag);
-			});
+        displayEventInfo = function (occurrence) {
+            $("#result").html(function () {
+                return "<h3>" + occurrence.event.name + "</h3>" + "<p>" + occurrence.event.description + "</p>" + "<h4>" + occurrence.venue.name + "</h4>" + "<p><b>Address:</b> " + occurrence.venue.address + "</p>" + "<p><b>Start:</b> " + occurrence.start + "</p>" + "<p><b>End:</b> " + occurrence.end + "</p>";
+            });
 
-			// Browser doesn't support Geolocation
-		} else {
-			browserSupportFlag = false;
-			handleNoGeolocation(browserSupportFlag);
-		}
+        },
 
-		var handleNoGeolocation = function(errorFlag) {
-			if (errorFlag == true) {
-				alert("Geolocation service failed.");
-				initialLocation = newyork;
-			} else {
-				alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-				initialLocation = siberia;
-				infowindow = new google.maps.InfoWindow({
-					map : map,
-					position : initialLocation,
-					content : 'Here\'s what\'s going on in Siberia...'
-				});
-			}
-			map.setCenter(initialLocation);
-		};
-	};
+        createMarker = function (occurrence) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(occurrence.venue.latitude, occurrence.venue.longitude),
+                title: occurrence.event.name,
+                animation: google.maps.Animation.DROP
+            });
+            google.maps.event.addListener(marker, 'click', function () {
+                displayInfoWindow(occurrence);
+                displayEventInfo(occurrence);
+            });
+            return marker;
+        },
 
-	var calculateRadius = function(bounds) {
-		if (typeof(Number.prototype.toRad) === "undefined") {
-			  Number.prototype.toRad = function() {
-			    return this * Math.PI / 180;
-			  }
-			}
-		var R = 3959, // Statute Mile
-		    center = bounds.getCenter(),
-		    corner = bounds.getNorthEast(),
-		    lat1 = center.lat().toRad(),
-		    lat2 = corner.lat().toRad(),
-		    dLat = (lat2-lat1).toRad(),
-		    dLon = (corner.lng()-center.lng()).toRad();
+        populateMap = function (gMap, events, mapMarkers) {
+            var i,
+                deleteOverlays = function () {
+                    if (mapMarkers) {
+                        for (i = 0; i < mapMarkers.length; i += 1) {
+                            mapMarkers[i].setMap(null);
+                        }
+                    }
+                    mapMarkers.length = 0;
+                };
+            map = gMap;
+            deleteOverlays();
+            for (i = 0; i < events.length; i += 1) {
+                mapMarkers.push(createMarker(events[i]));
+            }
+        },
 
-		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		return R * c;
-	};
+        parseOccurrencesForCalendar = function (occurrences) {
+            var calendarEvents = [],
+                i;
+            for (i = 0; i < occurrences.length; i += 1) {
+                calendarEvents.push({
+                    title: occurrences[i].event.name,
+                    start: new Date(occurrences[i].start),
+                    end: new Date(occurrences[i].end),
+                    event: occurrences[i].event,
+                    venue: occurrences[i].venue,
+                    allDay: false
 
-	var generateOccurrenceURL = function(center,radius,start,end) {
-		return 'http://localhost:8080/wutup/occurrences?page=0&pageSize=20&center=' +
-				center.lat() + ','+ center.lng() + '&radius=' +
-				(radius <= 100 ? radius : 100) + '&start=' + start.getTime() +
-				'&end=' + end.getTime();
-	};
+                });
+            }
+            return calendarEvents;
+        },
 
-	var populateMap = function(gMap, events, mapMarkers) {
-		var deleteOverlays = function () {
-			  if (mapMarkers) {
-			    for (i in mapMarkers) {
-			      mapMarkers[i].setMap(null);
-			    }
-			  }
-			  mapMarkers.length = 0;
-		};
-		map = gMap;
-		deleteOverlays();
-		for (var i = 0; i < events.length; i++) {
-			mapMarkers.push(createMarker(events[i]));
-		}
-	};
-	var createMarker = function(occurrence) {
-		var marker = new google.maps.Marker({
-			map : map,
-			position : new google.maps.LatLng(occurrence.venue.latitude, occurrence.venue.longitude),
-			title : occurrence.event.name,
-			animation : google.maps.Animation.DROP
-		});
-		google.maps.event.addListener(marker, 'click', function() {
-			displayInfoWindow(occurrence);
-			displayEventInfo(occurrence);
-		});
-		return marker;
-	}
-	var displayInfoWindow = function(occurrence) {
-		if (infowindow)
-			infowindow.close();
-		infowindow = new google.maps.InfoWindow({
-			content : occurrence.event.name + '</br>',
-			position : new google.maps.LatLng(occurrence.venue.latitude, occurrence.venue.longitude)
-		});
-		infowindow.open(map);
-	};
+        populateCalendar = function (calendarEvents) {
+            $('#calendar').fullCalendar('removeEventSource', calendarEvents);
+            $('#calendar').fullCalendar('removeEvents');
+            $('#calendar').fullCalendar('addEventSource', calendarEvents);
+        },
 
-	var displayEventInfo = function(occurrence) {
-		$("#result").html(function() {
-			return "<h3>" + occurrence.event.name + "</h3>" + "<p>" + occurrence.event.description + "</p>" + "<h4>" + occurrence.venue.name + "</h4>" + "<p><b>Address:</b> " + occurrence.venue.address + "</p>" + "<p><b>Start:</b> " + occurrence.start + "</p>" + "<p><b>End:</b> " + occurrence.end + "</p>";
-		});
+        createCalendar = function (calendarEvents, map, mapMarkers) {
+            var calendar = $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                theme: true,
+                editable: false,
+                disableDragging: true,
+                dayClick: function (date, allDay) {
+                    if (allDay) {
+                        calendar.fullCalendar('gotoDate', date.getFullYear(), date.getMonth(), date.getDate());
+                        calendar.fullCalendar('changeView', 'agendaDay');
+                    }
+                },
+                viewDisplay: function (view) { // This function controls behavior when the calendar view is changed
+                    var bounds = map.getBounds(),
+                        radius = calculateRadius(bounds);
+                    $.getJSON(generateOccurrenceURL(bounds.getCenter(), radius, view.start, view.end), function (occurrences) {
+                        calendarEvents = parseOccurrencesForCalendar(occurrences);
+                        populateCalendar(calendarEvents);
+                        populateMap(map, calendarEvents, mapMarkers);
+                    });
+                },
+                eventClick: function (event) { //'event' is used here instead of 'occurrence' due to fullcalendar.js documentation
+                    displayInfoWindow(event);
+                    displayEventInfo(event);
+                },
+                events: calendarEvents
+            });
+        },
 
-	};
+        getCalendarView = function () {
+            return $('#calendar').fullCalendar('getView');
+        },
 
-	var parseOccurrencesForCalendar = function(occurrences) {
-		var calendarEvents = [];
-		for (var i = 0; i < occurrences.length; i++) {
-			calendarEvents.push({
-				title : occurrences[i].event.name,
-				start : new Date(occurrences[i].start),
-				end : new Date(occurrences[i].end),
-				event : occurrences[i].event,
-				venue : occurrences[i].venue,
-				allDay : false
+        instantiateMapAndCalendar = function () {
+            var initialLocation, siberia = new google.maps.LatLng(60, 105),
+                newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687),
+                browserSupportFlag = false,
+                calendarEvents = null,
+                mapOptions = {
+                    zoom: 10,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                },
+                mapMarkers = [],
+                handleNoGeolocation = function (errorFlag) {
+                    if (errorFlag === true) {
+                        alert("Geolocation service failed.");
+                        initialLocation = newyork;
+                    } else {
+                        alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+                        initialLocation = siberia;
+                        infowindow = new google.maps.InfoWindow({
+                            map: map,
+                            position: initialLocation,
+                            content: 'Here\'s what\'s going on in Siberia...'
+                        });
+                    }
+                    map.setCenter(initialLocation);
+                };
+            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-			});
-		}
-		return calendarEvents;
-	};
+            // Trying W3C Geolocation
+            if (navigator.geolocation) {
+                browserSupportFlag = true;
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    map.setCenter(initialLocation);
+                    google.maps.event.addListenerOnce(map, 'idle', function () {
+                        createCalendar(calendarEvents, map, mapMarkers);
+                    });
+                    var bounds, radius, center;
+                    google.maps.event.addListener(map, 'idle', function () {
+                        bounds = map.getBounds();
+                        center = bounds.getCenter();
+                        radius = calculateRadius(bounds);
+                        $.getJSON(generateOccurrenceURL(center, radius, getCalendarView().start, getCalendarView().end), function (occurrences) {
+                            calendarEvents = parseOccurrencesForCalendar(occurrences);
+                            populateCalendar(calendarEvents);
+                            populateMap(map, calendarEvents, mapMarkers);
+                        });
 
-	var createCalendar = function(calendarEvents, map, mapMarkers) {
-		var calendar = $('#calendar').fullCalendar({
-			header : {
-				left : 'prev,next today',
-				center : 'title',
-				right : 'month,agendaWeek,agendaDay'
-			},
-			theme : true,
-			editable : false,
-			disableDragging : true,
-			dayClick : function(date, allDay, jsEvent, view) {
-				if (allDay) {
-					calendar.fullCalendar('gotoDate', date.getFullYear(), date.getMonth(), date.getDate());
-					calendar.fullCalendar('changeView', 'agendaDay');
-				}
-			},
-			viewDisplay: function(view) { // This function controls behavior when the calendar view is changed
-			        var bounds = map.getBounds();
-			        var	radius = calculateRadius(bounds);
-			        $.getJSON(generateOccurrenceURL(bounds.getCenter(),radius,view.start,view.end), function(occurrences) {
-						calendarEvents = parseOccurrencesForCalendar(occurrences);
-						populateCalendar(calendarEvents);
-						populateMap(map, calendarEvents, mapMarkers);
-					});
-		    },
-			eventClick : function(event, jsEvent, view) {//'event' is used here instead of 'occurrence' due to fullcalendar.js documentation
-				displayInfoWindow(event);
-				displayEventInfo(event);
-			},
-			events : calendarEvents
-		});
-	};
+                    });
 
-	var populateCalendar = function(calendarEvents) {
-		$('#calendar').fullCalendar('removeEventSource', calendarEvents);
-		$('#calendar').fullCalendar('removeEvents');
-		$('#calendar').fullCalendar('addEventSource', calendarEvents);
-	};
+                }, function () {
+                    handleNoGeolocation(browserSupportFlag);
+                });
 
-	var getCalendarView = function() {
-		return	$('#calendar').fullCalendar('getView');
-	};
-	instantiateMapAndCalendar();
-	//Start your Engines!
+                // Browser doesn't support Geolocation
+            } else {
+                browserSupportFlag = false;
+                handleNoGeolocation(browserSupportFlag);
+            }
+
+        };
+
+
+    instantiateMapAndCalendar();
+    //Start your Engines!
 
 });
