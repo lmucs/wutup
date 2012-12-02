e7e23eaf89715e519737c0c22d34be50abd79eca
Marc Mouallem <Marc.4D@gmail.com>
2012-12-02 14:41:15 -0800

    Added logging and comments to ManualListAdapter class (Android client).

--- android/app/src/edu/lmu/cs/wutup/android/manager/LogTags.java
+++ android/app/src/edu/lmu/cs/wutup/android/manager/LogTags.java
@@ -3,5 +3,6 @@ package edu.lmu.cs.wutup.android.manager;
 public class LogTags {
 
 	public static final String EVENT_CREATION = "Event Creation";
+	public static final String ADAPTER = "Adapter";
 	
 }
