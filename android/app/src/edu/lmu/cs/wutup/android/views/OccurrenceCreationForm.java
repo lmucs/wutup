6dc6ebe242ffcf43a4075f4144f92f329edb5891
Marc Mouallem <Marc.4D@gmail.com>
2012-12-02 22:12:56 -0800

    Created methods in event creation activity for extracting Unix time from widgets. Also added a create occurrence and cancel button to the occurrence creation layout (Android client).

--- android/app/src/edu/lmu/cs/wutup/android/views/OccurrenceCreationForm.java
+++ android/app/src/edu/lmu/cs/wutup/android/views/OccurrenceCreationForm.java
@@ -3,6 +3,10 @@ package edu.lmu.cs.wutup.android.views;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
+import android.widget.CalendarView;
+import android.widget.TimePicker;
+
+import org.joda.time.DateTimeConstants;
 
 import edu.lmu.cs.wutup.android.autofill.DynamicSearchTrigger;
 import edu.lmu.cs.wutup.android.autofill.ManualListAdapter;
@@ -11,13 +15,13 @@ import edu.lmu.cs.wutup.android.manager.R;
 import edu.lmu.cs.wutup.android.model.Event;
 import edu.lmu.cs.wutup.android.model.Venue;
 
-
 public class OccurrenceCreationForm extends Activity {
 	
 /**********************************************************************************************************************
  * Member Variables BEGIN
  **********************************************************************************************************************/
 
+	
 	private static final int NUMBER_OF_CHARACTERS_REQUIRED_BEFORE_OFFERING_SUGGESTIONS = 1;
 	
 	AutoCompleteTextView eventTextField;
@@ -25,6 +29,14 @@ public class OccurrenceCreationForm extends Activity {
 	
 	AutoCompleteTextView venueTextField;
 	ManualListAdapter<Venue> venueAdapter;
+	
+	CalendarView startDate;
+	TimePicker startTime;
+	CalendarView endDate;
+	TimePicker endTime;
+	
+	long startInUnixTime;
+	long endInUnixTime;
 
 /**********************************************************************************************************************
  * Member Variables END & Android Life Cycle Methods BEGIN
@@ -38,6 +50,11 @@ public class OccurrenceCreationForm extends Activity {
 		
 		initiateEventDynamicSearch();
 		initiateVenueDynamicSearch();
+		
+		startDate = (CalendarView) findViewById(R.id.occurrence_creation_form_date_start);
+		startTime = (TimePicker) findViewById(R.id.occurrence_creation_form_time_start);
+		endDate = (CalendarView) findViewById(R.id.occurrence_creation_form_date_end);
+		endTime = (TimePicker) findViewById(R.id.occurrence_creation_form_time_end);
 		
 	}
 	
@@ -74,6 +91,28 @@ public class OccurrenceCreationForm extends Activity {
 		venueTextField.addTextChangedListener(new DynamicSearchTrigger<Venue>(Venue.class, 
 				                                                              venueAdapter, 
 				                                                              HttpWutup.ADDRESS_OF_VENUES));
+		
+	}
+	
+	private void calculateAndSetStartInUnixTime() {
+		
+		long unixTime = startDate.getDate();
+		
+		unixTime += startTime.getCurrentHour() * DateTimeConstants.MILLIS_PER_HOUR;
+		unixTime += startTime.getCurrentMinute() * DateTimeConstants.MILLIS_PER_MINUTE;
+		
+		startInUnixTime = unixTime;
+		
+	}
+	
+	private void calculateAndSetEndInUnixTime() {
+		
+		long unixTime = endDate.getDate();
+		
+		unixTime += endTime.getCurrentHour() * DateTimeConstants.MILLIS_PER_HOUR;
+		unixTime += endTime.getCurrentMinute() * DateTimeConstants.MILLIS_PER_MINUTE;
+		
+		endInUnixTime = unixTime;
 		
 	}
 	
