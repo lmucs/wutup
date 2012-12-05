77d11fa79ab3e9a1fe9e31cd01dbd19b9368872e
Sam <verhasseltsamuelj@gmail.com>
2012-12-04 16:58:49 -0800

    Created an About Page to explain Wutup.

---
+++ webclient/src/main/java/edu/lmu/cs/wutup/AboutPage.java
@@ -0,0 +1,14 @@
+package edu.lmu.cs.wutup;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class AboutPage extends PageWrapper {
+	private static final long serialVersionUID = 1L;
+
+    public AboutPage (final PageParameters parameters) {
+	super(parameters);
+
+		// TODO Add your page's components here
+
+	}
+}
